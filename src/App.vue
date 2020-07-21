a3b931db5682e7955f10359cf1c0e8f05d764866
eason920 <eason920@gmail.com>
2020-07-21 22:14:01 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>h1 from app.vue</h1>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created(){
+    this.$http.get('https://randomuser.me/api/')
+      .then((res)=>{
+        console.log('res', res.data.results[0]);
+      })
+  }
+}
+</script>
+
+<style>
+h1{color: blue!important;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import axios from 'axios' // 主要 ajax 套件
+import VueAxios from 'vue-axios' // 將它(ajax)轉為 vue 的套件，使可用 this 指令引用
+
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
